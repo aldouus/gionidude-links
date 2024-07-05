345fce614e2172bfab3cb75c7c2b577cede9d105
Aldous <144833094+aldouus@users.noreply.github.com>
2024-07-05 12:30:05 +0200

    Remove analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -17,13 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Script defer src="https://cloud.umami.is/script.js" data-website-id="0ac100d7-c16b-4c7a-a4bf-2851843b7ada" />
     <html lang="en">
       <body className={inter.className}>
         {children}
       </body>
     </html>
-    </>
   );
 }
